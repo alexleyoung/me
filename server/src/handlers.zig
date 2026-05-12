d121263a92e7fe97106fa887d266ae8db7b4498b
Alex Young <alexyoung0119@gmail.com>
2026-05-12 14:48:42 -0500

    fixes

--- server/src/handlers.zig
+++ server/src/handlers.zig
@@ -1,17 +1,18 @@
 const std = @import("std");
 
+const main = @import("main.zig");
 const http = @import("http.zig");
 
-pub fn handle_req(req: http.Request) !void {
+pub fn handle(ctx: main.Context, conn: std.Io.net.Stream, req: http.Request) !void {
     switch (req.method) {
         .GET => {
             var writer_buf: [8192]u8 = undefined;
-            var writer = conn.writer(io, &writer_buf);
+            var writer = conn.writer(ctx.io, &writer_buf);
             try writer.interface.print("{s} 200 OK\r\nContent-Length: {d}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n", .{
-                req.version.get(&reader_buf),
-                index.len,
+                "HTTP/1.1",
+                ctx.pages.index.len,
             });
-            try writer.interface.writeAll(index);
+            try writer.interface.writeAll(ctx.pages.index);
             try writer.interface.flush();
         },
         else => {},
--- server/src/main.zig
+++ server/src/main.zig
@@ -30,24 +30,27 @@ pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const gpa = init.gpa;
 
+    const ctx = try Context.init(io, gpa);
+
     const srv = try server.Server.init(io, "127.0.0.1", 7878);
     var listener = try srv.listen();
     defer listener.deinit(io);
 
     while (true) {
         const conn = try listener.accept(io);
-        try handle_conn(io, conn);
+        try handleConn(ctx, conn);
     }
 }
 
-fn handle_conn(io: std.Io, conn: std.Io.net.Stream) !void {
-    defer conn.close(io);
+fn handleConn(ctx: Context, conn: std.Io.net.Stream) !void {
+    defer conn.close(ctx.io);
 
     var reader_buf: [8192]u8 = undefined;
-    var reader = conn.reader(io, &reader_buf);
+    var reader = conn.reader(ctx.io, &reader_buf);
     defer reader.interface.tossBuffered();
 
-    // TODO: actually handle this error
+    // TODO: actually handle this error(s)
     const req = try http.readRequest(&reader.interface);
-    try handlers.handle_req(req);
+    std.debug.print("handling {} request\n", .{req.method});
+    try handlers.handle(ctx, conn, req);
 }
